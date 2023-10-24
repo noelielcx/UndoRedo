886dc76cdcd77b1dd1beaee07a05cf78aee731aa
Noëlie Lacroix <98475894+noelielcx@users.noreply.github.com>
2023-10-24 15:44:47 +0200

    fini2

--- pages/polylineUndo/polylineUndo.js
+++ pages/polylineUndo/polylineUndo.js
@@ -1,7 +1,60 @@
 import Stack from './stack';
 import Konva from "konva";
 import { createMachine, interpret } from "xstate";
-import UndoManager from './UndoManager.js'
+class UndoManager{
+    constructor(buttonUndo, buttonRedo){
+        this.buttonUndo = buttonUndo
+        this.buttonRedo = buttonRedo
+        this.undoStack = new Stack()
+        this.redoStack = new Stack()
+    }
+
+    getUndoStack(){
+        return this.undoStack;
+    }
+
+    getRedoStack(){
+        return this.redoStack;
+    }
+
+    execute(command){
+        this.undoStack.push(command)
+        this.buttonUndo.disabled = this.canUndo()
+        this.buttonRedo.disabled = this.canRedo()
+    }
+
+    undo(){
+        let command = this.undoStack.peek()
+        command.undo()
+        this.redoStack.push(command)
+        this.undoStack.pop()
+        this.buttonUndo.disabled = this.canUndo()
+        this.buttonRedo.disabled = this.canRedo()
+    }
+
+    canUndo(){
+        if(!this.undoStack.isEmpty()){
+            return false
+        }
+        return true
+    }
+
+    redo(){
+        let command = this.redoStack.peek()
+        command.redo()
+        this.undoStack.push(command)
+        this.redoStack.pop()
+        this.buttonUndo.disabled = this.canUndo()
+        this.buttonRedo.disabled = this.canRedo()
+    }
+
+    canRedo(){
+        if(!this.redoStack.isEmpty()){
+            return false
+        }
+        return true
+    }
+}
 class UndoCommand {
     constructor(polyline, dessin){
         this.polyline = polyline
